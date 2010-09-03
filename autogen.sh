0504d60c103fb136432a7681281f7423b66bc7f4
kongo2002 <kongo2002@googlemail.com>
2010-09-03 12:22:20 +0200

    modify .gitignores

--- autogen.sh
+++ autogen.sh
@@ -6,8 +6,11 @@ if test -d .git; then
         git log --pretty=medium | fold -s > ChangeLog
 fi
 
-libtoolize --automake
 aclocal -I m4
+
+libtoolize --automake
+intltoolize --automake
+
 autoheader
 automake --add-missing --copy
 autoconf
