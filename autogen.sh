aa6e0f32981a8517f9af36aeb02afdbec386cac0
kongo2002 <kongo2002@googlemail.com>
2010-09-03 15:06:14 +0200

    further autogen.sh modification

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,12 @@
 # Create a ChangeLog file if necessary
 if test -d .git; then
     test -e ChangeLog ||
-        git log --pretty=medium | fold -s > ChangeLog
+        git log --date-order --date=short |\
+            sed -e '/^commit.*$/d' |\
+            awk '/^Author/ {sub(/\\$/, ""); getline t; print $0 t; next}; 1' |\
+            sed -e 's/^Author: //g' |\
+            sed -e 's/>Date:   \([0-9]*-[0-9]*-[0-9]*\)/>\t\1/g' |\
+            sed -e 's/^[ \t]\+/\t* /g' > ChangeLog
 fi
 
 aclocal -I m4
